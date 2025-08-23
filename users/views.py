1512efc2883db81fd76734d50b6717c92bd13d69
Petr Maev <petr.maev94@gmail.com>
2025-08-23 12:55:36 +0300

    added views for users

---
+++ users/views.py
@@ -0,0 +1,38 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from users.models import CustomUser
+from users.permissions import IsModer, IsUserOwner
+from users.serializers import CustomUserSerializer
+
+
+class UserRegisterAPIView(generics.CreateAPIView):
+    serializer_class = CustomUserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated, IsUserOwner]
+
+
+class UserListAPIView(generics.ListCreateAPIView):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated, IsModer]
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated, IsUserOwner]
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [IsAuthenticated, IsUserOwner]
