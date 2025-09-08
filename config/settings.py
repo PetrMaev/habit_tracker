36aea400e1710c0d588b7fe0227564274888a7ed
Petr Maev <petr.maev94@gmail.com>
2025-09-08 20:55:24 +0300

    added db_host in host

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         "NAME": os.getenv("DATABASE_NAME"),
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
+        "HOST": os.getenv("DATABASE_HOST", "DB_HOST"),
         "PORT": os.getenv("DATABASE_PORT"),
     }
 }
